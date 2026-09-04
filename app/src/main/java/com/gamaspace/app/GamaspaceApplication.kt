8b23a9aaf2644cc1aff41e7d8131c9851a8b8636
coco0081 <castroalex.41anzu@gmail.com>
2026-09-04 13:37:10 -0500

    Add Application class with Hilt support

---
+++ app/src/main/java/com/gamaspace/app/GamaspaceApplication.kt
@@ -0,0 +1,10 @@
+package com.gamaspace.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Clase Application con soporte para Hilt
+ */
+@HiltAndroidApp
+class GamaspaceApplication : Application()
